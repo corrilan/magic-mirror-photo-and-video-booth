d7306ba0adfefac6992ccfb41389ea0188d0647d
corrilan <33758018+corrilan@users.noreply.github.com>
2019-04-23 17:25:50 +0100

    Update manual-lychee-upload.sh

--- scripts/manual-lychee-upload.sh
+++ scripts/manual-lychee-upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python /home/pi/photo-video-booth-code/lycheeupload/lycheeupload.py -d /home/pi/photo-video-booth-code/photo-archive/ --port 2222 --verbose jbassett@jason-and-lynsey.com:/var/www/customersites/www.jason-and-lynsey.com/Lychee/
+python /home/pi/magic-mirror-photo-and-video-booth/lycheeupload/lycheeupload.py -d /home/pi/magic-mirror-photo-and-video-booth/photo-archive/ --port 2222 --verbose jbassett@jason-and-lynsey.com:/var/www/customersites/www.jason-and-lynsey.com/Lychee/
 
