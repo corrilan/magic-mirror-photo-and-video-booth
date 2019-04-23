e5c99800e4ee5e82fd7fe9056d82d8fa9d7c2a7a
corrilan <33758018+corrilan@users.noreply.github.com>
2019-04-23 17:26:16 +0100

    Update prepare-for-stream-to-twitch.sh

--- prepare-for-stream-to-twitch.sh
+++ prepare-for-stream-to-twitch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/pi/photo-video-booth-code/FFmpeg/ffmpeg -thread_queue_size 512 -threads 3 -ar 44100 -ac 2 -f alsa -i hw:1,0 -f v4l2 -thread_queue_size 512 -itsoffset 1 -codec:v h264 -framerate 30 -video_size 1920x1080 -i /dev/video0 -copyinkf -codec:v copy -codec:a aac -ab 128k -g 10 -rtsp_transport tcp -t 20 -f flv rtmp://live-lhr.twitch.tv/app/live_your_own-live_key_here
+/home/pi/magic-mirror-photo-and-video-booth/FFmpeg/ffmpeg -thread_queue_size 512 -threads 3 -ar 44100 -ac 2 -f alsa -i hw:1,0 -f v4l2 -thread_queue_size 512 -itsoffset 1 -codec:v h264 -framerate 30 -video_size 1920x1080 -i /dev/video0 -copyinkf -codec:v copy -codec:a aac -ab 128k -g 10 -rtsp_transport tcp -t 20 -f flv rtmp://live-lhr.twitch.tv/app/live_your_own-live_key_here
